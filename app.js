2cc9dd4e00aa6696869688849767b61c54851f47
Daria Rutts <darya.rutts@yandex.ru>
2023-07-10 14:20:25 +0300

    ci: создать схемы и модели для пользователя и карточки

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const { PORT = 3000 } = process.env;
 const app = express();
 
 app.get("/", (_req, res) => {
-  res.send('Hello there');
+  res.send("Hello there");
 });
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+})
 
 app.listen(PORT, () => {
   console.log(`App is running on port ${PORT}`);
