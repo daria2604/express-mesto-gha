b219774969993b7609782e7a78b22604ca52b568
Daria Rutts <darya.rutts@yandex.ru>
2023-07-11 19:36:27 +0300

    refactor: добавить роуты пользователя для изменения профиля и аватара

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,10 @@
 const User = require('../models/user');
 
+const options = {
+  new: true, // обработчик then получит на вход обновлённую запись
+  runValidators: true, // данные будут валидированы перед изменением
+};
+
 const getUsers = (req, res) => {
   User.find({}).then((users) => {
     res.send(users);
@@ -17,4 +22,18 @@ const createUser = (req, res) =>
     res.status(201).send(user);
   });
 
-module.exports = { getUsers, getUser, createUser };
+const updateProfile = (req, res) => {
+  const { name, about } = req.body;
+  User.findByIdAndUpdate(req.user._id, { name, about }, options).then((user) =>
+    res.status(200).send(user)
+  );
+};
+
+const updateAvatar = (req, res) => {
+  const { avatar } = req.body;
+  User.findByIdAndUpdate(req.user._id, { avatar }, options).then((user) =>
+    res.status(200).send(user)
+  );
+};
+
+module.exports = { getUsers, getUser, createUser, updateProfile, updateAvatar };
