5676631f614b8d78790730977a9799adf6e7f756
Daria Rutts <darya.rutts@yandex.ru>
2023-07-12 14:18:29 +0300

    refactor: добавить обработчики ошибок

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,24 +1,45 @@
 const Card = require('../models/card');
 
 const getCards = (req, res) => {
-  Card.find({}).then((cards) => {
-    res.send(cards);
-  });
+  Card.find({})
+    .then((cards) => {
+      res.send(cards);
+    })
+    .catch((err) => {
+      res.status(500).send({ message: 'Произошла ошибка в работе сервера.' });
+    });
 };
 
 const createCard = (req, res) => {
   const { name, link } = req.body;
   const owner = req.user._id;
 
-  Card.create({ name, link, owner }).then((card) => {
-    res.status(201).send(card);
-  });
+  Card.create({ name, link, owner })
+    .then((card) => {
+      res.status(201).send(card);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({
+          message: 'Переданы некорректные данные при создании карточки.',
+        });
+      }
+      res.status(500).send({ message: 'Произошла ошибка в работе сервера.' });
+    });
 };
 
 const deleteCard = (req, res) => {
-  Card.findByIdAndRemove(req.params.cardId).then((card) => {
-    res.status(200).send(card);
-  });
+  Card.findByIdAndRemove(req.params.cardId)
+    .then((card) => {
+      res.status(200).send(card);
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res
+          .status(404)
+          .send({ message: 'Карточка с указанным _id не найдена.' });
+      }
+    });
 };
 
 const likeCard = (req, res) =>
@@ -26,17 +47,45 @@ const likeCard = (req, res) =>
     req.params.cardId,
     { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
     { new: true }
-  ).then((card) => {
-    res.status(200).send(card);
-  });
+  )
+    .then((card) => {
+      res.status(200).send(card);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({
+          message: 'Переданы некорректные данные для постановки лайка.',
+        });
+      } else if (err.name === 'CastError') {
+        res
+          .status(404)
+          .send({ message: 'Передан несуществующий _id карточки.' });
+      } else {
+        res.status(500).send({ message: 'Произошла ошибка в работе сервера.' });
+      }
+    });
 
 const unlikeCard = (req, res) =>
   Card.findByIdAndUpdate(
     req.params.cardId,
     { $pull: { likes: req.user._id } }, // убрать _id из массива
     { new: true }
-  ).then((card) => {
-    res.status(200).send(card);
-  });
+  )
+    .then((card) => {
+      res.status(200).send(card);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({
+          message: 'Переданы некорректные данные для снятия лайка.',
+        });
+      }
+      if (err.name === 'CastError') {
+        res
+          .status(404)
+          .send({ message: 'Передан несуществующий _id карточки.' });
+      }
+      res.status(500).send({ message: 'Произошла ошибка в работе сервера.' });
+    });
 
 module.exports = { getCards, createCard, deleteCard, likeCard, unlikeCard };
--- controllers/users.js
+++ controllers/users.js
@@ -6,34 +6,80 @@ const options = {
 };
 
 const getUsers = (req, res) => {
-  User.find({}).then((users) => {
-    res.send(users);
-  });
+  User.find({})
+    .then((users) => {
+      res.status(200).send(users);
+    })
+    .catch((err) => {
+      res.status(500).send({ message: 'Произошла ошибка в работе сервера.' });
+    });
 };
 
 const getUser = (req, res) => {
-  User.findById(req.params.userId).then((user) => {
-    res.status(200).send(user);
-  });
+  User.findById(req.params.userId)
+    .then((user) => {
+      res.status(200).send(user);
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res
+          .status(404)
+          .send({ message: 'Пользователь по указанному _id не найден.' });
+      }
+      res.status(500).send({ message: 'Произошла ошибка в работе сервера.' });
+    });
 };
 
 const createUser = (req, res) =>
-  User.create({ ...req.body }).then((user) => {
-    res.status(201).send(user);
-  });
+  User.create({ ...req.body })
+    .then((user) => {
+      res.status(201).send(user);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({
+          message: 'Переданы некорректные данные при создании пользователя.',
+        });
+      }
+      res.status(500).send({ message: 'Произошла ошибка в работе сервера.' });
+    });
 
 const updateProfile = (req, res) => {
   const { name, about } = req.body;
-  User.findByIdAndUpdate(req.user._id, { name, about }, options).then((user) =>
-    res.status(200).send(user)
-  );
+  User.findByIdAndUpdate(req.user._id, { name, about }, options)
+    .then((user) => res.status(200).send(user))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({
+          message: 'Переданы некорректные данные при обновлении профиля.',
+        });
+      }
+      if (err.name === 'CastError') {
+        res
+          .status(404)
+          .send({ message: 'Пользователь по указанному _id не найден.' });
+      }
+      res.status(500).send({ message: 'Произошла ошибка в работе сервера.' });
+    });
 };
 
 const updateAvatar = (req, res) => {
   const { avatar } = req.body;
-  User.findByIdAndUpdate(req.user._id, { avatar }, options).then((user) =>
-    res.status(200).send(user)
-  );
+  User.findByIdAndUpdate(req.user._id, { avatar }, options)
+    .then((user) => res.status(200).send(user))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({
+          message: 'Переданы некорректные данные при обновлении аватара.',
+        });
+      }
+      if (err.name === 'CastError') {
+        res
+          .status(404)
+          .send({ message: 'Пользователь по указанному _id не найден.' });
+      }
+      res.status(500).send({ message: 'Произошла ошибка в работе сервера.' });
+    });
 };
 
 module.exports = { getUsers, getUser, createUser, updateProfile, updateAvatar };
