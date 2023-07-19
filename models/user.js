671a8c6e7ce73d9e7e5805d52d535d23eec1f25f
Daria Rutts <darya.rutts@yandex.ru>
2023-07-19 13:36:34 +0300

    refactor: присваивать стандартные значения name, about, avatar, если они незаполнены

--- models/user.js
+++ models/user.js
@@ -5,19 +5,20 @@ const userSchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      required: [true, 'Поле "name" должно быть заполнено'],
       minlength: [2, 'Минимальная длина поля "name" - 2'],
       maxlength: [30, 'Максимальная длина поля "name" - 30'],
+      default: 'Жак-Ив Кусто',
     },
     about: {
       type: String,
-      required: [true, 'Поле "about" должно быть заполнено'],
       minlength: [2, 'Минимальная длина поля "about" - 2'],
       maxlength: [30, 'Максимальная длина поля "about" - 30'],
+      default: 'Исследователь океана',
     },
     avatar: {
       type: String,
-      required: true,
+      default:
+        'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
     },
     email: {
       type: String,
