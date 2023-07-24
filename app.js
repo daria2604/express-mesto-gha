7a02009467f6a68ecb09b5d93912b61bc3e0e2b3
Daria Rutts <darya.rutts@yandex.ru>
2023-07-24 11:46:26 +0300

    refactor: изменить максимальное количество запросов

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.en
 const app = express();
 const limiter = rateLimit({
 	windowMs: 15 * 60 * 1000, // 15 minutes
-	max: 3, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
   message: { message: limitErrorMessage },
 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
